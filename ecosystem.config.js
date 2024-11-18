dfd3de3e495cc3e49b1e0b25d176ec8ecb3b3379
Niskarsh <niskarshk@gmail.com>
2024-11-18 17:50:43 +0530

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: "miscellaneous.proofofskill.org",
+        script: "npm start",
+        // args: "start",
+        time: true,
+        autorestart: true,
+      },
+    ]
+  };
